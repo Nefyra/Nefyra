be8003ae1a1d746370848ba90e1438475727747b
Nefyra <nefyran@gmail.com>
2025-03-02 01:31:04 +0800

    nefyra

---
+++ NefyraExo/app/src/main/java/com/nefyra/exo/VideoConfig.java
@@ -0,0 +1,13 @@
+package com.nefyra.exo;
+
+public class VideoConfig {
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
